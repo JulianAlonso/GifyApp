92af785d89231e3ebd97ba6ebe0682b105204189
Julian <julian.alonso.dev@gmail.com>
2015-03-10 16:27:08 +0100

    Added GifAPIClientRequestManager

---
+++ GifyApp/RequestManagers/GifAPIClientRequestManager.h
@@ -0,0 +1,13 @@
+//
+//  GifAPIClientRequestManager.h
+//  GifyApp
+//
+//  Created by Julian Alonso on 10/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GifAPIClientRequestManager : NSObject
+
+@end
---
+++ GifyApp/RequestManagers/GifAPIClientRequestManager.m
@@ -0,0 +1,13 @@
+//
+//  GifAPIClientRequestManager.m
+//  GifyApp
+//
+//  Created by Julian Alonso on 10/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "GifAPIClientRequestManager.h"
+
+@implementation GifAPIClientRequestManager
+
+@end
