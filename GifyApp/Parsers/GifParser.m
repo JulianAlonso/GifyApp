1d475ce1ec5553472ffde850bfe2aa5c4d9097c3
Julian <julian.alonso.dev@gmail.com>
2015-03-10 18:33:27 +0100

    Added gifParser

---
+++ GifyApp/Parsers/GifParser.m
@@ -0,0 +1,13 @@
+//
+//  GifParser.m
+//  GifyApp
+//
+//  Created by Julian Alonso on 10/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "GifParser.h"
+
+@implementation GifParser
+
+@end
