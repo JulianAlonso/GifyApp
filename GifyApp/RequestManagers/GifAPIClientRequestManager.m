dc0ac96257c6e51b9e96fd340f3fd53755788dbc
Julian <julian.alonso.dev@gmail.com>
2015-03-10 17:16:52 +0100

    End login in clien request manager for search request

--- GifyApp/RequestManagers/GifAPIClientRequestManager.m
+++ GifyApp/RequestManagers/GifAPIClientRequestManager.m
@@ -16,14 +16,24 @@
 - (void)searchGifWithWords:(NSArray *)words completionBlock:(void (^)(NSData *))completionBlock
 {
     
-    NSDictionary *params = [NSDictionary dictionary];
+    NSDictionary *params = [ParamsMakerHelper paramsWithArrays:@[words, @[kApiKey]] forKeys:@[@"q", @"api_key"]];
     
     NSURL *url = [URLMakerHelper urlWithBasepath:kApiBasePath endPoint:kSearchPath andParams:params];
     
     NSURLSession *session = [NSURLSession sharedSession];
     
-    
-    
+    [[session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+
+        if (error)
+        {
+            completionBlock(nil);
+        }
+        else
+        {
+            completionBlock(data);
+        }
+        
+    }] resume];
 }
 
 @end
