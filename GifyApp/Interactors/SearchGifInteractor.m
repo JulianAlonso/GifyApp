bb4a62d6cae2371c17c5a10dc3d136f659d7fcd4
Julian <julian.alonso.dev@gmail.com>
2015-03-10 15:27:04 +0100

    Added search interactor

---
+++ GifyApp/Interactors/SearchGifInteractor.m
@@ -0,0 +1,13 @@
+//
+//  SearchGifInteractor.m
+//  GifyApp
+//
+//  Created by Julian Alonso on 10/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import "SearchGifInteractor.h"
+
+@implementation SearchGifInteractor
+
+@end
