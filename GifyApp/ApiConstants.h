c822b304073e8342fe6391763b4a20d809a0085f
Julian <julian.alonso.dev@gmail.com>
2015-03-10 15:41:52 +0100

    Added constants header file

---
+++ GifyApp/ApiConstants.h
@@ -0,0 +1,14 @@
+//
+//  ApiConstants.h
+//  GifyApp
+//
+//  Created by Julian Alonso on 10/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#ifndef GifyApp_ApiConstants_h
+#define GifyApp_ApiConstants_h
+
+#define kUrlApiBase = @""
+
+#endif
